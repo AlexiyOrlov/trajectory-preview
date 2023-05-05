dc4d5d8f80dd0235ad8b4854b43c0ded48cbfe58
alexiy <alexiy.ov@gmail.com>
2023-05-05 23:03:30 +0300

    Remove unneeded

--- src/main/java/dev/buildtool/trajectory/preview/TrajectoryPreview.java
+++ src/main/java/dev/buildtool/trajectory/preview/TrajectoryPreview.java
@@ -4,8 +4,6 @@ import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -14,21 +12,14 @@ import org.apache.logging.log4j.Logger;
 public class TrajectoryPreview {
     static ForgeConfigSpec.ConfigValue<String> firstColor, secondColor;
     static ForgeConfigSpec.IntValue trajectoryStart;
-    // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
     public TrajectoryPreview() {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, new ForgeConfigSpec.Builder().configure(builder -> {
             firstColor = builder.define("Odd point color", "75aaff");
             secondColor = builder.define("Even point color", "e7ed49");
             trajectoryStart = builder.defineInRange("Start preview after such distance", 2, 1, 256);
             return builder.build();
         }).getRight());
-    }
-
-    private void setup(final FMLCommonSetupEvent event) {
-
     }
 }
