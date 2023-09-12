2e90642df9a3f79877f9d150c3818789f4042ae2
alexiy <alexiy.ov@gmail.com>
2023-09-12 15:13:43 +0300

    Fix freeze

--- src/main/java/dev/buildtool/trajectory/preview/CrossbowArrowPreview.java
+++ src/main/java/dev/buildtool/trajectory/preview/CrossbowArrowPreview.java
@@ -20,8 +20,6 @@ import net.minecraft.world.item.ArrowItem;
 import net.minecraft.world.item.CrossbowItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.item.enchantment.EnchantmentHelper;
-import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
@@ -98,10 +96,6 @@ public class CrossbowArrowPreview extends Entity implements PreviewEntity<Abstra
 
         abstractarrow.setSoundEvent(SoundEvents.CROSSBOW_HIT);
         abstractarrow.setShotFromCrossbow(true);
-        int i = EnchantmentHelper.getItemEnchantmentLevel(Enchantments.PIERCING, crossbow);
-        if (i > 0) {
-            abstractarrow.setPierceLevel((byte) i);
-        }
 
         return abstractarrow;
     }
